8ada2241b966ec077207c531f5cddbc4210f846d
Szymon Sadkowski <ss406325@students.mimuw.edu.pl>
2019-06-08 18:49:38 +0200

    gitfix

--- src/map_main.c
+++ src/map_main.c
@@ -20,73 +20,73 @@
 }
 
 int main() {
-	Interpreter *interpreter = Interpreter_new();
-	if(interpreter == NULL) {
-		return 0;
-	}
-	Map *map = newMap();
-	if(map == NULL) {
-		Interpreter_remove(interpreter);
-		return 0;
-	}
-	Vector *commands = Vector_new(Command_remove);
-	if(commands == NULL) {
-		Interpreter_remove(interpreter);
-		deleteMap(map);
-		return 0;
-	}
+    Interpreter *interpreter = Interpreter_new();
+    if(interpreter == NULL) {
+        return 0;
+    }
+    Map *map = newMap();
+    if(map == NULL) {
+        Interpreter_remove(interpreter);
+        return 0;
+    }
+    Vector *commands = Vector_new(Command_remove);
+    if(commands == NULL) {
+        Interpreter_remove(interpreter);
+        deleteMap(map);
+        return 0;
+    }
 
-	ADD_COMMAND(commands, _doesRawInputMatchAddRoad, _sanitizeInputAddRoad, _validateInputsAddRoad, _executeAddRoad)
-	ADD_COMMAND(commands, _doesRawInputMatchRepairRoad, _sanitizeInputRepairRoad, _validateInputsRepairRoad, _executeRepairRoad)
-	ADD_COMMAND(commands,_doesRawInputMatchGetRouteDesc, _sanitizeInputGetRouteDesc, _validateInputsGetRouteDesc, _executeGetRouteDesc)
-	ADD_COMMAND(commands, _doesRawInputMatchNewCustomRoute, _sanitizeInputNewCustomRoute, _validateInputsNewCustomRoute, _executeNewCustomRoute)
-	ADD_COMMAND(commands, _doesRawInputMatchNewRoute, _sanitizeInputNewRoute, _validateInputsNewRoute, _executeNewRoute)
-	ADD_COMMAND(commands, _doesRawInputMatchExtendRoute, _sanitizeInputExtendRoute, _validateInputsExtendRoute, _executeExtendRoute)
-	ADD_COMMAND(commands, _doesRawInputMatchRemoveRoad, _sanitizeInputRemoveRoad, _validateInputsRemoveRoad, _executeRemoveRoad)
-	ADD_COMMAND(commands, _doesRawInputMatchRemoveRoute, _sanitizeInputRemoveRoute, _validateInputsRemoveRoute, _executeRemoveRoute)
+    ADD_COMMAND(commands, _doesRawInputMatchAddRoad, _sanitizeInputAddRoad, _validateInputsAddRoad, _executeAddRoad)
+    ADD_COMMAND(commands, _doesRawInputMatchRepairRoad, _sanitizeInputRepairRoad, _validateInputsRepairRoad, _executeRepairRoad)
+    ADD_COMMAND(commands,_doesRawInputMatchGetRouteDesc, _sanitizeInputGetRouteDesc, _validateInputsGetRouteDesc, _executeGetRouteDesc)
+    ADD_COMMAND(commands, _doesRawInputMatchNewCustomRoute, _sanitizeInputNewCustomRoute, _validateInputsNewCustomRoute, _executeNewCustomRoute)
+    ADD_COMMAND(commands, _doesRawInputMatchNewRoute, _sanitizeInputNewRoute, _validateInputsNewRoute, _executeNewRoute)
+    ADD_COMMAND(commands, _doesRawInputMatchExtendRoute, _sanitizeInputExtendRoute, _validateInputsExtendRoute, _executeExtendRoute)
+    ADD_COMMAND(commands, _doesRawInputMatchRemoveRoad, _sanitizeInputRemoveRoad, _validateInputsRemoveRoad, _executeRemoveRoad)
+    ADD_COMMAND(commands, _doesRawInputMatchRemoveRoute, _sanitizeInputRemoveRoute, _validateInputsRemoveRoute, _executeRemoveRoute)
 
-	String *rawInput;
-	while ((rawInput = Interpreter_getLine(interpreter)) != NULL) {
-		bool isCommandMatched = false;
-		int status = 0;
-		Command *currentCmd = NULL;
-		for (size_t i = 0; i < Vector_getSize(commands) && !isCommandMatched; i++) {
-			currentCmd = (Command *) Vector_getElemById(commands, i);
-			status = Command_doesRawInputMatchTemplate(currentCmd, rawInput);
-			if (status >= 0) {
-				isCommandMatched = (status > 0);
-			}
-			else {
-				/* free all memory, end program */
-				goto free_all;
-			}
-		}
-		int status2;
-		if (!isCommandMatched) {
-			fprintf( stderr, "ERROR %lu\n", Interpreter_getCurrentLine(interpreter));
-			status = 0;
-		}
-		if(status && (status2 = Command_validateInput(currentCmd, rawInput)) < 0) {
-			goto free_all;
-		}
-		if (status && status2 == 0) {
-			fprintf( stderr, "ERROR %lu\n", Interpreter_getCurrentLine(interpreter));
-			status = 0;
-		}
-		if(status && (status2 = Command_execute(currentCmd, map)) < 0) {
-			goto free_all;
-		}
-		if(status && status2 == 0) {
-			fprintf( stderr, "ERROR %lu\n", Interpreter_getCurrentLine(interpreter));
-		}
-		Command_reset(currentCmd);
-		String_remove(rawInput);
-	}
+    String *rawInput;
+    while ((rawInput = Interpreter_getLine(interpreter)) != NULL) {
+        bool isCommandMatched = false;
+        int status = 0;
+        Command *currentCmd = NULL;
+        for (size_t i = 0; i < Vector_getSize(commands) && !isCommandMatched; i++) {
+            currentCmd = (Command *) Vector_getElemById(commands, i);
+            status = Command_doesRawInputMatchTemplate(currentCmd, rawInput);
+            if (status >= 0) {
+                isCommandMatched = (status > 0);
+            }
+            else {
+                /* free all memory, end program */
+                goto free_all;
+            }
+        }
+        int status2;
+        if (!isCommandMatched) {
+            fprintf( stderr, "ERROR %lu\n", Interpreter_getCurrentLine(interpreter));
+            status = 0;
+        }
+        if(status && (status2 = Command_validateInput(currentCmd, rawInput)) < 0) {
+            goto free_all;
+        }
+        if (status && status2 == 0) {
+            fprintf( stderr, "ERROR %lu\n", Interpreter_getCurrentLine(interpreter));
+            status = 0;
+        }
+        if(status && (status2 = Command_execute(currentCmd, map)) < 0) {
+            goto free_all;
+        }
+        if(status && status2 == 0) {
+            fprintf( stderr, "ERROR %lu\n", Interpreter_getCurrentLine(interpreter));
+        }
+        Command_reset(currentCmd);
+        String_remove(rawInput);
+    }
 
-	free_all:
-	Interpreter_remove(interpreter);
-	deleteMap(map);
-	Vector_remove(commands);
+    free_all:
+    Interpreter_remove(interpreter);
+    deleteMap(map);
+    Vector_remove(commands);
 
-	return 0;
+    return 0;
 }
